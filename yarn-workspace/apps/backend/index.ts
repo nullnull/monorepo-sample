26b9a3daebb46f9bd78cedccc94c6b973923fe07
Katsuma Narisawa <katsuma.narisawa@gmail.com>
2022-07-22 14:07:14 +0900

    Import local packages on backend

--- yarn-workspace/apps/backend/index.ts
+++ yarn-workspace/apps/backend/index.ts
@@ -1,12 +1,15 @@
 import express from 'express'
+import { functionA } from '@monorepo-sample/a'
 
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  const x = functionA()
+  res.send(`Hello World! ${x}`)
 })
 
 app.listen(port, () => {
+  functionA()
   console.log(`Example app listening on port ${port}`)
 })
