bbd09093f6eff477061921c6241109b27ff25a53
Katsuma Narisawa <katsuma.narisawa@gmail.com>
2022-07-22 17:20:50 +0900

    [monorepo-sample] @monorepo-sample/aをnext-transpile-modulesの対象に追加

--- yarn-workspace/apps/frontend/next.config.js
+++ yarn-workspace/apps/frontend/next.config.js
@@ -1,5 +1,5 @@
 /** @type {import('next').NextConfig} */
-const withTM = require("next-transpile-modules")(["@monorepo-sample/ui"]);
+const withTM = require("next-transpile-modules")(["@monorepo-sample/ui", "@monorepo-sample/a"]);
 
 module.exports = withTM({
   reactStrictMode: true,
