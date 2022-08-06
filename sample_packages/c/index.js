9a9c4898ac389069a1d18f3c640ca8ba69c52a42
Katsuma Narisawa <katsuma.narisawa@gmail.com>
2022-08-06 17:46:57 +0900

    c v0.0.4

--- sample_packages/c/index.js
+++ sample_packages/c/index.js
@@ -3,6 +3,6 @@ exports.__esModule = true;
 exports.functionC = void 0;
 var katsumanarisawa_a_1 = require("katsumanarisawa-a");
 function functionC() {
-    return "functionC v0.0.2 a: " + katsumanarisawa_a_1.functionA();
+    return "functionC v0.0.4 a: " + katsumanarisawa_a_1.functionA() + " must be v.0.0.6";
 }
 exports.functionC = functionC;
--- sample_packages/c/index.ts
+++ sample_packages/c/index.ts
@@ -1,5 +1,5 @@
 import { functionA } from "katsumanarisawa-a";
 
 export function functionC() {
-  return `functionC v0.0.2 a: ${functionA()}`
+  return `functionC v0.0.4 a: ${functionA()} must be v.0.0.6`
 }
