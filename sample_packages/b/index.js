abb1ea807e189426ddcdbf5c7088a4d683f00909
Katsuma Narisawa <katsuma.narisawa@gmail.com>
2022-08-06 17:47:39 +0900

    b v0.0.5

--- sample_packages/b/index.js
+++ sample_packages/b/index.js
@@ -3,6 +3,6 @@ exports.__esModule = true;
 exports.functionB = void 0;
 var katsumanarisawa_a_1 = require("katsumanarisawa-a");
 function functionB() {
-    return "functionB v0.0.3 a: " + katsumanarisawa_a_1.functionA();
+    return "functionB v0.0.5 a: " + katsumanarisawa_a_1.functionA() + " must be 0.0.5";
 }
 exports.functionB = functionB;
--- sample_packages/b/index.ts
+++ sample_packages/b/index.ts
@@ -1,5 +1,5 @@
 import { functionA } from "katsumanarisawa-a";
 
 export function functionB() {
-  return `functionB v0.0.4 a: ${functionA()} must be 0.0.5`
+  return `functionB v0.0.5 a: ${functionA()} must be 0.0.5`
 }
